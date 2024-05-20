c2765e66e1e1d820c7320f34344e02528f1cb68a
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-20 12:29:25 +0530

    hero form fixed

--- src/components/Home/HeroForm.jsx
+++ src/components/Home/HeroForm.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Services from "./Services";
 import axios from "axios";
+import "./heroform.css";
 
 const HeroForm = ({ oneline }) => {
   const [formData, setFormData] = useState({
@@ -10,7 +11,7 @@ const HeroForm = ({ oneline }) => {
     companyName: "",
     websiteUrl: "",
     email: "",
-    fromWhere: "Digital marketing services in mumbai",
+    fromWhere: "Digital marketing services in dallas",
   });
 
   const handleChange = (e) => {
@@ -64,95 +65,76 @@ const HeroForm = ({ oneline }) => {
           onSubmit={handleSubmit}
         >
           <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
-            <div className="w-full relative  group mt-[23px]  ">
+            <div class="user-box mt-[23px] ">
               <input
+                className=""
                 type="text"
-                id="name"
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
                 required
-                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500  border-b border-gray-500"
               />
-              <label
-                htmlFor="name"
-                className=" transform transition-all absolute  top-0 left-0   flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[18px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
-              >
+              <label htmlFor="name" className="">
                 Name
               </label>
             </div>
-            <div className="w-full relative group mt-[23px]">
+            <div class="user-box mt-[23px] ">
               <input
+                className=""
                 type="text"
-                id="phoneNo"
                 name="phoneNo"
                 value={formData.phoneNo}
                 onChange={handleChange}
                 required
-                className="w-full h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500  border-b border-gray-500"
               />
-              <label
-                htmlFor="phoneNo"
-                className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[10px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
-              >
+              <label htmlFor="phoneNo" className="">
                 Phone No
               </label>
             </div>
           </div>
-
-          <div className="w-full relative group mt-[23px]">
+          <div class="user-box mt-[23px] ">
             <input
+              className=""
               type="text"
               name="companyName"
-              id="companyName"
               value={formData.companyName}
               onChange={handleChange}
               required
-              className="w-full h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500  border-b border-gray-500"
             />
-            <label
-              htmlFor="companyName"
-              className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[10px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
-            >
+            <label htmlFor="companyName" className="">
               Company Name
             </label>
           </div>
-          <div className="w-full relative group mt-[23px]">
+
+          <div class="user-box mt-[23px] ">
             <input
+              className=""
               type="text"
               name="websiteUrl"
-              id="websiteUrl"
               value={formData.websiteUrl}
               onChange={handleChange}
               required
-              className="w-full h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500  border-b border-gray-500"
             />
-            <label
-              htmlFor="websiteUrl"
-              className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[10px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
-            >
+            <label htmlFor="websiteUrl" className="">
               Website Url
             </label>
           </div>
-          <div className="w-full relative group mt-[23px]">
+
+          <div class="user-box mt-[23px] ">
             <input
+              className=""
               type="text"
-              id="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               required
-              className="w-full h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500  border-b border-gray-500"
             />
-            <label
-              htmlFor="email"
-              className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[10px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
-            >
+            <label htmlFor="email" className="">
               Email
             </label>
           </div>
 
-          <button className="py-3 rounded-xl bg-[#e31a20] mt-4 w-full  font-poppins text-white text-[16px] md:text-xl ">
+          <button className="py-3 rounded-xl bg-[#e31a20] mt-5 w-full  font-poppins text-white text-[16px] md:text-xl ">
             SEND REQUEST
           </button>
         </form>
